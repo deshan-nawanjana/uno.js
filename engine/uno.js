7e79237d02911ec67daf007a09698b249158fe8d
Deshan Nawanjana <deshan.uok@gmail.com>
2022-07-05 17:29:19 +0530

    Add New Methods and Update to Single Transmission

--- engine/uno.js
+++ engine/uno.js
@@ -1,30 +1,42 @@
+// pin mode constants
+const OUTPUT = 0
+const INPUT = 1
+
+// volt level constants
+const HIGH = 1
+const LOW = 0
+
+// main class
 const UNO = class {
     
     constructor() {
 
-        // handler values
-        this.port = null
+        // port object
+        let port = null
         // running state flag
-        this.running = false
+        let running = false
         // supported versions
-        const versions = ['1.0.0']
+        const versions = ['1.0.22']
 
         // init method
         this.init = async function() {
             return new Promise(resolve => {
                 // request usb port
-                navigator.serial.requestPort().then(port => {
-                    // store port
-                    this.port = port
+                navigator.serial.requestPort().then(portObject => {
+                    // store port object
+                    port = portObject
                     // opening port
-                    port.open({ baudRate : 1000000 }).then(() => {
+                    port.open({ baudRate : 2000000 }).then(() => {
+                        // get reader and writer
+                        send.reader = port.readable.getReader()
+                        send.writer = port.writable.getWriter()
                         // success request delay 
                         setTimeout(() => {
                             // request client version
-                            this.getVersion().then(ver => {
+                            getVersion().then(ver => {
                                 if(versions.includes(ver)) {
                                     // set running flag
-                                    this.running = true
+                                    running = true
                                     // callback resolve
                                     resolve()
                                 } else {
@@ -32,8 +44,6 @@ const UNO = class {
                                     console.log('Please update the UNO.js Client.')
                                 }
                             })
-                            // start communication
-                            communicate()
                         }, 3000)
                     })
                 })
@@ -42,120 +52,111 @@ const UNO = class {
 
         // pin states
         const states = { digital : [], analog : [] }
-        // pending requests
-        let requests = []
-        // bytes remainder
-        let remainder = []
 
-        this.states = states
+        // sender busy state
+        let busy = false
 
-        // communication method
-        const communicate = () => {
-            // get reader and writer
-            const reader = uno.port.readable.getReader()
-            const writer = uno.port.writable.getWriter()
-            // loop method
-            const loop = () => {
-                // check for pending requests to send
-                const req = requests.find(x => x.sent === false)
-                // is any available request and no ongoing read
-                if(req !== undefined && remainder.length === 0) {
-                    // create array for bytes
-                    const arr = [req.type].concat(req.data)
-                    // push end character
-                    arr.push(255)
-                    //send message
-                    writer.write(new Uint8Array(arr)).then(() => {
-                        // mark as sent
-                        req.sent = true
-                        // loop again
-                        requestAnimationFrame(loop)
-                    })
-                } else if(requests.find(x => x.sent === true)) {
-                    // read for bytes
-                    reader.read().then(obj => {
+        // first state flag
+        let first = true
+
+        const types = [ 201, 202, 203, 204, 205, 206, 207, 208, 209, 210 ]
+
+        const send = async function(type, resolve, reject, data = []) {
+            // return if busy
+            if(busy) { return reject() }
+            // set busy flag
+            busy = true
+            // create array for buffer
+            const array = [type].concat(data)
+            // push end character
+            array.push(255)
+            // send message as buffer
+            send.writer.write(new Uint8Array(array)).then(() => {
+                // bytes remainder
+                let remainder = []
+                // read loop until end character
+                const readLoop = () => {
+                    // read message
+                    send.reader.read().then(obj => {
                         // get received bytes
                         const arr = Array.from(obj.value)
                         // combine with remainder
                         remainder = remainder.concat(arr)
-                        // clearing remainder from overflows
-                        clearRemainder()
-                        // get any end character
+                        // clear garbage in remainder
+                        remainder = readClear(remainder)
+                        // get end character index
                         const end = remainder.indexOf(255)
                         // callback for completed message
                         if(end > -1) {
                             // get message from remainder
                             const data = remainder.splice(0, end)
-                            // clear remainder
-                            remainder.splice(0, 1)
-                            // test
-                            callbackCommunication(data)
+                            // resolve callback
+                            sendResolve(data, resolve)
+                            // release busy flag
+                            busy = false
+                        } else {
+                            // read loop again
+                            readLoop()
                         }
-                        // loop again
-                        requestAnimationFrame(loop)
-                    })
-                } else {
-                    // loop again
-                    requestAnimationFrame(loop)
+                    }).catch(reject)
                 }
-            }
-            // start loop
-            loop()
-        }
-
-        // request codes other than pin states
-        let types = [201, 202, 204, 205]
-
-        // clear remainder garbage
-        const clearRemainder = () => {
-            if(remainder.some(x => types.includes(x))) { return }
-            if(remainder.indexOf(203) !== remainder.lastIndexOf(203)) {
-                if(remainder.includes(255)) {
-                    const index = remainder.lastIndexOf(255)
-                    remainder.splice(0, index + 1)
+                // read clear method for garbage
+                const readClear = arr => {
+                    // no message type in front byte
+                    if(types.includes(arr[0]) === false) {
+                        // check for end character
+                        if(arr.includes(255)) {
+                            // splice from end
+                            arr.splice(0, arr.lastIndexOf(255) + 1)
+                        } else if(arr.length > 1) {
+                            // clear all due to garbage
+                            arr = []
+                        }
+                    }
+                    // first call check
+                    if(first === true && arr[0] !== 201 && arr[arr.length - 1] === 255) {
+                        // clear array
+                        arr = []
+                    }
+                    // update flag
+                    first = false
+                    // return remainder
+                    return arr
                 }
-            }
+                // start read loop
+                readLoop()
+            }).catch(reject)
         }
 
-        // communication callback method
-        const callbackCommunication = data => {
-            // get message type
-            const type = data.shift()
-            // find matching request to resolve
-            const req = requests.find(x => x.type === type)
-            if(req) {
-                // remove request from requests
-                requests = requests.filter(x => x !== req)
-                // callback resolve after updaing pin states
-                req.resolve(extractPinStates(data))
-            }
-        }
-
-        // pin state update method
-        const extractPinStates = data => {
+        const sendResolve = (data, resolve) => {
+            // remove message type
+            data.shift()
             // get digital states
             const digital = data.splice(0, data.indexOf(254) + 1)
             // get analog states
-            const analog = data.splice(0, data.indexOf(254) + 1).map(x => parseInt(x * 5.12))
+            const analog = data.splice(0, data.indexOf(254) + 1).map(x => parseInt(x * 5.115))
             // update digital pins
             states.digital = digital.splice(0, digital.length - 1)
             // update analog pins
             states.analog = analog.splice(0, analog.length - 1)
-            // return rest result
-            return data
+            // callback resolve after updaing pin states
+            resolve(data)
         }
 
         // get version method
-        this.getVersion = async function() {
-            return new Promise(resolve => {
-                requests.push({
-                    type : 201,
-                    data : [],
-                    sent : false,
-                    resolve : arr => {
-                        resolve(arr.join('.'))
-                    }
-                })
+        const getVersion = async function() {
+            return new Promise((resolve, reject) => {
+                send(201, arr => {
+                    resolve(arr.splice(0, 3).join('.'))
+                }, reject)
+            })
+        }
+
+        // update pin states method
+        this.update = async function() {
+            // return promise
+            return new Promise((resolve, reject) => {
+                send(203, resolve, reject, [])
             })
         }
 
@@ -188,7 +189,7 @@ const UNO = class {
                     data[par_1] = []
                 }
             }
-            // output object
+            // output array
             const out = []
             // push other pins
             for(let i = 0; i < states.digital.length; i++) {
@@ -200,7 +201,33 @@ const UNO = class {
                     out.push(0)
                 } else {
                     // no change to pin
-                    out.push(2)
+                    out.push(253)
+                }
+            }
+            // return output
+            return out
+        }
+
+        // analog pin conveter method
+        const convertPinFormatAnalog = args => {
+            // data object
+            let data = typeof args[0] === 'object' ? args[0] : {}
+            // check arguments length
+            if(args.length === 2) {
+                data[args[0]] = args[1]
+            }
+            // output array
+            const out = []
+            // get keys array
+            const krr = Object.keys(data).map(x => parseInt(x))
+            // push other pins
+            for(let i = 0; i < states.digital.length; i++) {
+                if(krr.includes(i)) {
+                    // analog value in range 0  to 255 => 0 to 200
+                    out.push(parseInt((data[i] > 255 ? 255 : data[i]) / 1.275))
+                } else {
+                    // no change to pin
+                    out.push(253)
                 }
             }
             // return output
@@ -210,39 +237,95 @@ const UNO = class {
         // set pin mode method
         this.pinMode = async function() {
             // get pin format
-            const data = convertPinFormat(arguments, 'input', 'output')
+            const data = convertPinFormat(arguments, 'INPUT', 'OUTPUT')
             // return promise
-            return new Promise(resolve => {
-                requests.push({
-                    type : 202,
-                    data : data,
-                    sent : false,
-                    resolve : resolve
-                })
+            return new Promise((resolve, reject) => {
+                send(202, resolve, reject, data)
             })
         }
 
         // digital write method
         this.digitalWrite = async function() {
             // get pin format
-            const data = convertPinFormat(arguments, 'high', 'low')
+            const data = convertPinFormat(arguments, 'HIGH', 'LOW')
             // return promise
-            return new Promise(resolve => {
-                requests.push({
-                    type : 204,
-                    data : data,
-                    sent : false,
-                    resolve : resolve
-                })
+            return new Promise((resolve, reject) => {
+                send(204, resolve, reject, data)
+            })
+        }
+
+        // digital write method
+        this.analogWrite = async function() {
+            // get pin format
+            const data = convertPinFormatAnalog(arguments)
+            // return promise
+            return new Promise((resolve, reject) => {
+                send(205, resolve, reject, data)
+            })
+        }
+
+        const encoder = new TextEncoder()
+        const decoder = new TextDecoder()
+
+        // delay method
+        this.delay = async function(milliseconds) {
+            // time input to buffer
+            const data = Array.from(encoder.encode(milliseconds))
+            // return promise
+            return new Promise((resolve, reject) => {
+                send(206, resolve, reject, data)
+            })
+        }
+
+        // delay method
+        this.delayMicroseconds = async function(microseconds) {
+            // time input to buffer
+            const data = Array.from(encoder.encode(microseconds))
+            // return promise
+            return new Promise((resolve, reject) => {
+                send(207, resolve, reject, data)
+            })
+        }
+
+        // millis method
+        this.millis = async function() {
+            // return promise
+            return new Promise((resolve, reject) => {
+                send(208, arr => {
+                    // remove last char
+                    arr.pop()
+                    // decode and resolve
+                    resolve(parseInt(decoder.decode(new Uint8Array(arr))))
+                }, reject, [])
+            })
+        }
+
+        // micros method
+        this.micros = async function() {
+            // return promise
+            return new Promise((resolve, reject) => {
+                send(209, arr => {
+                    // remove last char
+                    arr.pop()
+                    // decode and resolve
+                    resolve(parseInt(decoder.decode(new Uint8Array(arr))))
+                }, reject, [])
+            })
+        }
+
+        // pulse in method
+        this.pulseIn = async function(pin, value) {
+            // return promise
+            return new Promise((resolve, reject) => {
+                send(210, arr => {
+                    // remove last char
+                    arr.pop()
+                    console.log(arr)
+                    resolve(parseInt(decoder.decode(new Uint8Array(arr))))
+                }, reject, [pin, value])
             })
         }
 
     }
 
 }
-
-const OUTPUT = 0
-const INPUT = 1
-
-const HIGH = 1
-const LOW = 0
